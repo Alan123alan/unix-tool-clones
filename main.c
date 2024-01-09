963106b0f15c1164fad6e4d0c513347fef1994ef
Alan123alan <alancastellanos2@hotmail.com>
2024-01-08 21:47:21 -0600

    counting words by whitespace counting +1, not working

--- main.c
+++ main.c
@@ -32,4 +32,33 @@ int main(int argc, char *argv[]){
     }
     printf("line count: %zu\n", line_count);
   }
+  if(strcmp(argv[1], "-w") == 0){
+    FILE *fptr;
+    fptr = fopen(argv[2], "r");
+    if(fptr == NULL){
+      printf("Error opening file '%s' to read.\n", argv[3]);
+      return -1;
+    }
+    char line_buffer[MAX_LENGTH];
+    size_t word_count = 0;
+    while(fgets(line_buffer, MAX_LENGTH, fptr)){
+      size_t index = 0;
+      // char current_char = line_buffer[index];
+      size_t space_count = 0;
+      while(line_buffer[index] != '\0'){
+        if(line_buffer[index] == ' '){
+          space_count += 1;
+        }
+        index++;
+      }
+      word_count += space_count + 1;
+      // char *token = strtok(line_buffer, " ");
+      // word_count += 1;
+      // while(token != NULL){
+      //   token = strtok(NULL, " ");
+      //   word_count += 1;
+      // }
+    }
+    printf("word count: %zu\n", word_count);
+  }
 }
