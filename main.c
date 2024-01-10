efdef5934da40d04e4d23812c2372f57763cf4dd
Alan123alan <alancastellanos2@hotmail.com>
2024-01-10 13:36:23 -0600

    added default execution of wcclone

--- main.c
+++ main.c
@@ -8,71 +8,89 @@
 #define MAX_LENGTH 2048
 
 size_t utf8len(const char *s);
+size_t get_byte_count(char *filename);
+size_t get_line_count(char *filename);
+size_t get_word_count(char *filename);
+size_t get_char_count(char *filename);
 
 int main(int argc, char *argv[]){
   if(strcmp(argv[1], "-c") == 0){
+    printf("%zu %s\n", get_byte_count(argv[2]), argv[2]);
+  }else if(strcmp(argv[1], "-l") == 0){
+   printf("%zu %s\n", get_line_count(argv[2]), argv[2]);
+  }else if(strcmp(argv[1], "-w") == 0){
+   printf("%zu %s\n", get_word_count(argv[2]), argv[2]);
+  }else if(strcmp(argv[1], "-m") == 0){
+	  printf("%zu %s\n", get_char_count(argv[2]), argv[2]);
+  }else{
+    printf("%zu %zu %zu %s\n", get_line_count(argv[1]), get_word_count(argv[1]), get_byte_count(argv[1]), argv[1]);
+  }
+}
+
+size_t get_byte_count(char *filename){
     FILE *fptr;
-    fptr = fopen(argv[2],"r"); 
+    fptr = fopen(filename, "r"); 
     if(fptr == NULL){
-      printf("Error opening file '%s' to read.\n", argv[2]);
-      return -1;
+      printf("Error opening file '%s' to read.\n", filename);
+      return 1;
     }
     char line_buffer[MAX_LENGTH];
     size_t char_count = 0;
     while(fgets(line_buffer, MAX_LENGTH, fptr)){
       char_count += strlen(line_buffer);
     }
-	  printf("byte count: %zu\n", char_count);
-  }
-  if(strcmp(argv[1], "-l") == 0){
+    return char_count;
+}
+
+size_t get_line_count(char *filename){
     FILE *fptr;
-    fptr = fopen(argv[2], "r");
+    fptr = fopen(filename, "r");
     if(fptr == NULL){
-      printf("Error opening file '%s' to read.\n", argv[2]);
-      return -1;
+      printf("Error opening file '%s' to read.\n", filename);
+      return 1;
     }
     char line_buffer[MAX_LENGTH];
     size_t line_count = 0;
     while(fgets(line_buffer, MAX_LENGTH, fptr)){
       line_count += 1;
     }
-    printf("line count: %zu\n", line_count);
-  }
-  if(strcmp(argv[1], "-w") == 0){
+    return line_count;
+}
+
+size_t get_word_count(char *filename){
     FILE *fptr;
-    fptr = fopen(argv[2], "r");
+    fptr = fopen(filename, "r");
     if(fptr == NULL){
-      printf("Error opening file '%s' to read.\n", argv[3]);
-      return -1;
+      printf("Error opening file '%s' to read.\n", filename);
+      return 1;
     }
     wchar_t line_buffer[MAX_LENGTH];
     size_t word_count = 0;
     while(fgetws(line_buffer, MAX_LENGTH, fptr)){
-      // wchar_t *args[] = {"wc",line_buffer,NULL};
-      // execv("/usr/bin/wc",args);
       for(int i = 0; line_buffer[i] != '\0'; i++){
         if(!iswspace(line_buffer[i]) && iswspace(line_buffer[i+1])){
             word_count += 1;
         }
       }
     }
-    printf("%zu\n", word_count);
-  }
-  if(strcmp(argv[1], "-m") == 0){
+    fclose(fptr);
+    return word_count;
+}
+
+size_t get_char_count(char *filename){
     FILE *fptr;
-    fptr = fopen(argv[2],"r"); 
+    fptr = fopen(filename, "r"); 
     if(fptr == NULL){
-      printf("Error opening file '%s' to read.\n", argv[2]);
-      return -1;
+      printf("Error opening file '%s' to read.\n", filename);
+      return 1;
     }
     char line_buffer[MAX_LENGTH];
     size_t char_count = 0;
     while(fgets(line_buffer, MAX_LENGTH, fptr)){
       char_count += utf8len(line_buffer);
-      // char_count += wcslen(line_buffer);
     }
-	  printf("byte count: %zu\n", char_count);
-  }
+    fclose(fptr);
+    return char_count;
 }
 
 size_t utf8len(const char *s)
